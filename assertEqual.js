5b1880109460a2d21bf1970d64bb003535be5de9
marcellmueller <mlmueller@protonmail.com>
2020-10-26 21:44:55 +0000

    finished tail.js

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = (actual, expected) => {
   actual === expected
-    ? console.log(`🛑🛑🛑 Assertion Passed: [${actual}] === [${expected}]`)
-    : console.log(`✅✅✅ Assertion Failed: [${actual}] !== [${expected}]`);
+    ? console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`)
+    : console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
 };
 
 assertEqual('Lighthouse Labs', 'Bootcamp');
