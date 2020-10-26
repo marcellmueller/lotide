6af466ae791931e1119ba5db6ab6665c65f9daf2
marcellmueller <mlmueller@protonmail.com>
2020-10-26 19:46:05 +0000

    completed head.js

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`🛑🛑🛑 Assertion Passed: [${actual}] === [${expected}]`)
+    : console.log(`✅✅✅ Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+const head = (arr) => {
+  return arr[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+assertEqual(head([5, 6, 7]), 6);
+assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'World');
+assertEqual(head([]), 5);
