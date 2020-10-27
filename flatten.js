8b875fdad626f28749cdb86bc16eee30daddec85
marcellmueller <mlmueller@protonmail.com>
2020-10-27 15:55:33 -0700

    completed flatten.js

---
+++ flatten.js
@@ -0,0 +1,48 @@
+//function to compare two equal length arrays
+const compareArrays = (firstArray, secondArray) => {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//first checks if arrays are the same length then
+//calls compareArrays funtion if true
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length === secondArray.length) {
+    return compareArrays(firstArray, secondArray);
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = (firstArray, secondArray) => {
+  eqArrays(firstArray, secondArray)
+    ? console.log(
+        `✅✅✅ Assertion Passed: ${firstArray} and ${secondArray} are equal!`
+      )
+    : console.log(
+        `🛑🛑🛑 Assertion Failed: ${firstArray} does not equal ${secondArray}`
+      );
+};
+
+const flatten = (array) => {
+  const newArray = [];
+  for (const element of array) {
+    //check if element is an array
+    if (Array.isArray(element)) {
+      //loop through sub array and push to newArray
+      for (const num of element) {
+        newArray.push(num);
+      }
+    } else {
+      newArray.push(element);
+    }
+  }
+  return newArray;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, [3, 4], 5, [6]]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
