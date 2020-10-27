1629d47c2ceb70e142bc230453251a8a90acc0da
marcellmueller <mlmueller@protonmail.com>
2020-10-27 14:54:28 -0700

    added some notes

--- without.js
+++ without.js
@@ -30,6 +30,9 @@ const assertArraysEqual = (firstArray, secondArray) => {
       );
 };
 
+//without takes two arrays, a source array and a second with
+//items to remove. It then filters through items to remove and returns a
+//new array with the remaining items.
 const without = (sourceArray, itemsToRemove) => {
   let newArray = [...sourceArray];
   newArray = newArray.filter((i) => itemsToRemove.indexOf(i) === -1);
