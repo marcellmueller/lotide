2dae3f2a38e94b57e12def1943ab9fdc8aa9bbd4
marcellmueller <mlmueller@protonmail.com>
2020-10-27 13:52:30 -0700

    completed eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = (actual, expected) => {
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`)
+    : console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
+};
+
+//function to compare two equal length arrays
+const compareArrays = (firstArray, secondArray) => {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//first checks if arrays are the same length then
+//calls compareArrays funtion if true
+const eqArrays = (firstArray, secondArray) => {
+  if (firstArray.length === secondArray.length) {
+    return compareArrays(firstArray, secondArray);
+  } else {
+    return false;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
